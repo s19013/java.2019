2aa9d32587650b0ce87efda21710070e018a48c1
s19013 <s19013@std.it-college.ac.jp>
2019-11-26 21:37:27 +0900

    1

--- test/Ex5/Ex5.java
+++ test/Ex5/Ex5.java
@@ -1,9 +1,11 @@
 class Ex5{
 
 	public double ex5_1(int [] a){
-		
 		double ret = 0;
-		
+		for (int i = 0; i < a.length ;i++ ) {
+			ret += a[i];
+		}
+		ret /=a.length;
 		return ret;
 	}
 }
--- test/Ex7/Ex7.java
+++ test/Ex7/Ex7.java
@@ -1,65 +1,98 @@
 class Ex7{
 
 	public int ex7_1(int a, int b){
-		
-		return 0;
+		if (a <= b) {
+			return a;
+		}
+		else {
+			return b;
+		}
 	}
 
 	public int ex7_1(int a, int b, int c){
-		
-		return 0;
+		if (a<=b && a<=c) {
+			return a;
+		}
+		else if (b<=a && b<=c) {
+			return b;
+		}
+		else {
+			return c;
+		}
 	}
 
 	public int ex7_1(int [] a){
-		
-		return 0;
+		int[] arr = new int[a.length];
+		for( int i = 0; i < a.length; i++ ){
+			arr[i] = a[i] ;
+		}
+		int min = arr[0];
+		for (int i = 1; i < arr.length; i++) {
+		    int v = arr[i];
+		    if (v < min) {
+		        min = v;
+		    }
+		}
+		return min;
 	}
 
 	public int ex7_2(int x){
-		
+		if (x < 0) {
+			x *= -1;
+		}
+
 		return x;
 	}
 
 	public long ex7_2(long x){
-		
+		if (x < 0) {
+			x *= -1;
+		}
+
 		return x;
 	}
 
 	public float ex7_2(float x){
-		
+		if (x < 0) {
+			x *= -1;
+		}
+
 		return x;
 	}
 
 	public double ex7_2(double x){
-		
+		if (x < 0) {
+			x *= -1;
+		}
+
 		return x;
 	}
 
 	public String ex7_3(byte x){
-		
-		String ret = "";
-		
-		return ret;
+		String oct = Integer.toOctalString( x );
+		// String ret = x;
+
+		return oct;
 	}
 
 	public String ex7_3(short x){
-		
+
 		String ret = "";
-		
+
 		return ret;
 	}
 
 	public String ex7_3(int x){
-		
+
 		String ret = "";
-		
+
 		return ret;
 	}
 
 	public String ex7_3(long x){
-		
+
 		String ret = "";
-		
+
 		return ret;
 	}
 }
